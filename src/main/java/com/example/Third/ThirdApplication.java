35085991cb4c912650aa2c63366b2fd2717f972d
gajananpowar <95276036+gajananpowar@users.noreply.github.com>
2021-12-22 13:34:14 +0530

    initial commit:

---
+++ src/main/java/com/example/Third/ThirdApplication.java
@@ -0,0 +1,14 @@
+package com.example.Third;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdApplication.class, args);
+		System.out.println("fbbkh");
+	}
+
+}
